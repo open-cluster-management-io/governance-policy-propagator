35675e305f0c139dad08d5f6de34208efa311ea2
Yu Cao <ycao@redhat.com>
2020-04-28 23:18:18 -0400

    add status.placement and orphan replicated policy clean up

--- pkg/controller/propagator/propagation.go
+++ pkg/controller/propagator/propagation.go
@@ -6,6 +6,7 @@ import (
 
 	appsv1 "github.com/open-cluster-management/governance-policy-propagator/pkg/apis/apps/v1"
 	policiesv1 "github.com/open-cluster-management/governance-policy-propagator/pkg/apis/policies/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -15,6 +16,11 @@ import (
 
 func (r *ReconcilePolicy) handleRootPolicy(instance *policiesv1.Policy) error {
 	// flow -- if triggerred by user creating a new policy or updateing existing policy
+	if instance.Spec.Disabled {
+		// do nothing, clean up replicated policy
+		// deleteReplicatedPolicy
+		return nil
+	}
 	// 1. get binding
 	pbList := &policiesv1.PlacementBindingList{}
 	err := r.client.List(context.TODO(), pbList, &client.ListOptions{Namespace: instance.GetNamespace()})
@@ -27,6 +33,8 @@ func (r *ReconcilePolicy) handleRootPolicy(instance *policiesv1.Policy) error {
 		return nil
 	}
 	// 1.2 if exists -> step 2
+	placement := []*policiesv1.Placement{}
+	allDecisions := []appsv1.PlacementDecision{}
 	for _, pb := range pbList.Items {
 		if pb.Spec.Subject.APIGroup == policiesv1.SchemeGroupVersion.Group && pb.Spec.Subject.Kind == policiesv1.Kind && pb.Spec.Subject.Name == instance.GetName() {
 			plr := &appsv1.PlacementRule{}
@@ -38,16 +46,15 @@ func (r *ReconcilePolicy) handleRootPolicy(instance *policiesv1.Policy) error {
 				// reqLogger.Info("Failed to get plr, going to retry...")
 				return err
 			}
-			// plr found, update status.placement
-			// placement := policiesv1.Placement{PlacementBinding: decision.}
-			// r.client.Status().Patch(context.TODO())
+			// plr found, add current plcmnt to placement
+			placement = append(placement, &policiesv1.Placement{
+				PlacementBinding: pb.GetName(),
+				PlacementRule:    plr.GetName(),
+			})
 			// plr found, checking decision
 			decisions := plr.Status.Decisions
-			if decisions == nil || len(decisions) == 0 {
-				// no decision, skipping
-				return nil
-			}
 			for _, decision := range decisions {
+				allDecisions = append(allDecisions, decision)
 				// retrieve replicated policy in cluster namespace
 				replicatedPlc := &policiesv1.Policy{}
 				err := r.client.Get(context.TODO(), types.NamespacedName{Namespace: decision.ClusterNamespace, Name: instance.GetNamespace() + "." + instance.GetName()}, replicatedPlc)
@@ -74,23 +81,69 @@ func (r *ReconcilePolicy) handleRootPolicy(instance *policiesv1.Policy) error {
 							labels["cluster-namespace"] = decision.ClusterNamespace
 						}
 						replicatedPlc.SetLabels(labels)
-						return r.client.Create(context.TODO(), replicatedPlc)
+						err = r.client.Create(context.TODO(), replicatedPlc)
+						if err != nil {
+							return err
+						}
+					} else {
+						// failed to get replicated object, requeue
+						return err
 					}
-					// failed to get replicated object, requeue
-					return err
+
 				}
 				// replicated policy already created, need to compare and patch
-				return nil
+				// compare annotation
+				if !equality.Semantic.DeepEqual(instance.GetAnnotations(), replicatedPlc.GetAnnotations()) || !equality.Semantic.DeepEqual(instance.Spec, replicatedPlc.Spec) {
+					// update needed
+					replicatedPlc.SetAnnotations(instance.GetAnnotations())
+					replicatedPlc.Spec = instance.Spec
+					err = r.client.Update(context.TODO(), replicatedPlc)
+					if err != nil {
+						return err
+					}
+				}
 			}
 		}
 	}
 
-	// 2. check if placementrule and decision exists
+	// looped through all pb, update status.placement
+	instance.Status.Placement = placement
+	err = r.client.Status().Update(context.TODO(), instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// not found doing nothing, why? shouldn't happen?
+		}
+		// failed to update instance.spec.placement, requeue
+		return err
+	}
 
-	// 3. replicate
-	// 4. update status with binding and placementrule
-
-	// flow -- if triggerred by user updateing existing policy
+	// remove stale replicated policy based on allDecisions and status.status
+	// if cluster exists in status.status but doesn't exist in allDecistion, then it's stale cluster, we need to remove this replicated policy
+	for _, cluster := range instance.Status.Status {
+		found := false
+		for _, decision := range allDecisions {
+			if decision.ClusterName == cluster.ClusterName && decision.ClusterNamespace == cluster.ClusterNamespace {
+				found = true
+				break
+			}
+		}
+		// not found in allDecision, orphan, delete it
+		if !found {
+			err := r.client.Delete(context.TODO(), &policiesv1.Policy{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       policiesv1.Kind,
+					APIVersion: policiesv1.SchemeGroupVersion.Group,
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      instance.GetNamespace() + "." + instance.GetName(),
+					Namespace: cluster.ClusterNamespace,
+				},
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
@@ -130,14 +183,24 @@ func (r *ReconcilePolicy) handleReplicatedPolicy(instance *policiesv1.Policy) er
 		}
 	} else {
 		// loop through status and update
+		found := false
 		for _, compliancePerClusterStatus := range status {
 			if compliancePerClusterStatus.ClusterName == instance.GetLabels()["cluster-name"] && compliancePerClusterStatus.ClusterNamespace == instance.GetLabels()["cluster-namespace"] {
 				// found existing entry, check if it needs updating
+				found = true
 				if compliancePerClusterStatus.ComplianceState != instance.Status.ComplianceState {
 					compliancePerClusterStatus.ComplianceState = instance.Status.ComplianceState
 				}
 				break
 			}
+		}
+		// not found, it's a CompliancePerClusterStatus, add it
+		if !found {
+			status = append(status, &policiesv1.CompliancePerClusterStatus{
+				ComplianceState:  instance.Status.ComplianceState,
+				ClusterName:      instance.GetLabels()["cluster-name"],
+				ClusterNamespace: instance.GetLabels()["cluster-namespace"],
+			})
 		}
 	}
 	rootPls.Status.Status = status
