115b51f562576c20df1d0f3f3fc75f610459ac9d
Yu Cao <ycao@redhat.com>
2022-01-21 12:21:31 -0500

    Ensure reconcile requests are generated correctly (#158)
    When a placementbinding references multiple subjects, the mapper should
    always loop through entire subjects to generate reconcile request even when a
    policyset couldn't be retrieved.
    
    Signed-off-by: Yu Cao <ycao@redhat.com>

--- controllers/propagator/placementDecisionMapper.go
+++ controllers/propagator/placementDecisionMapper.go
@@ -67,7 +67,10 @@ func placementDecisionMapper(c client.Client) handler.MapFunc {
 						policySet := &policiesv1.PolicySet{}
 						err := c.Get(context.TODO(), policySetNamespacedName, policySet)
 						if err != nil {
-							return nil
+							log.V(2).Info("Failed to retrieve policyset referenced in placementbinding",
+								"policySetName", subject.Name, "placementBindingName", pb.GetName(), "error", err)
+
+							continue
 						}
 						policies := policySet.Spec.Policies
 						for _, plc := range policies {
