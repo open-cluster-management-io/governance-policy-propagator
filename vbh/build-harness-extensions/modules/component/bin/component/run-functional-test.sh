535a638af59df59c45cccae0c160dc1c541f9b9f
Dale Haiducek <dhaiduce@redhat.com>
2022-02-03 17:23:16 -0500

    Remove build harness (#163)
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- vbh/build-harness-extensions/modules/component/bin/component/run-functional-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Please specify a COMPONENT_TYPE when running component/test/functional to select the type of component you're building, or implement your own run-functional-tests.sh and set COMPONENT_BUILD_COMMAND to that file's path.";
-exit 1;
