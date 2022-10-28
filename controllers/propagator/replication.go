85b31b2e3882a1e34fe6a0e7f5d71a2173a84128
Justin Kulikauskas <jkulikau@redhat.com>
2022-10-28 11:47:11 -0400

    Fix some issues in replication code
    The new-ish functions are now tested (and fixed for problems the tests
    and reviewers found), and better documented. Hopefully some of the
    "fake" functions in the new tests can find some more use in the future.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- controllers/propagator/replication.go
+++ controllers/propagator/replication.go
@@ -2,6 +2,7 @@ package propagator
 
 import (
 	"context"
+	"strings"
 
 	k8sdepwatches "github.com/stolostron/kubernetes-dependency-watches/client"
 	"k8s.io/apimachinery/pkg/api/equality"
@@ -42,6 +43,9 @@ func equivalentReplicatedPolicies(plc1 *policiesv1.Policy, plc2 *policiesv1.Poli
 	return equality.Semantic.DeepEqual(plc1.Spec, plc2.Spec)
 }
 
+// buildReplicatedPolicy constructs a replicated policy based on a root policy and a placementDecision.
+// In particular, it adds labels that the policy framework uses, and ensures that policy dependencies
+// are in a consistent format suited for use on managed clusters.
 func (r *PolicyReconciler) buildReplicatedPolicy(
 	root *policiesv1.Policy, decision appsv1.PlacementDecision,
 ) (*policiesv1.Policy, error) {
@@ -84,13 +88,30 @@ func (r *PolicyReconciler) buildReplicatedPolicy(
 	return replicated, nil
 }
 
+// depIsPolicySet returns true if the given PolicyDependency is a PolicySet
+func depIsPolicySet(dep policiesv1.PolicyDependency) bool {
+	return dep.Kind == policiesv1.PolicySetKind &&
+		dep.APIVersion == policiesv1beta1.GroupVersion.String()
+}
+
+// depIsPolicy returns true if the given PolicyDependency is a Policy
+func depIsPolicy(dep policiesv1.PolicyDependency) bool {
+	return dep.Kind == policiesv1.Kind &&
+		dep.APIVersion == policiesv1.GroupVersion.String()
+}
+
+// canonicalizeDependencies returns an adjusted list of the input dependencies, ensuring that
+// Policies are in a consistent format (omitting the namespace, and using the <namespace>.<name>
+// format as in replicated Policies), and that PolicySets are replaced with their constituent
+// Policies. If a PolicySet could not be found, that dependency will be copied as-is. It will
+// return an error if there is an unexpected error looking up a PolicySet to replace.
 func (r *PolicyReconciler) canonicalizeDependencies(
 	rawDeps []policiesv1.PolicyDependency, defaultNamespace string,
 ) ([]policiesv1.PolicyDependency, error) {
 	deps := make([]policiesv1.PolicyDependency, 0)
 
 	for _, dep := range rawDeps {
-		if dep.Kind == policiesv1.PolicySetKind && dep.APIVersion == common.APIGroup+"/v1beta1" {
+		if depIsPolicySet(dep) {
 			plcset := &policiesv1beta1.PolicySet{}
 
 			if dep.Namespace == "" {
@@ -118,25 +139,28 @@ func (r *PolicyReconciler) canonicalizeDependencies(
 				deps = append(deps, policiesv1.PolicyDependency{
 					TypeMeta: v1.TypeMeta{
 						Kind:       policiesv1.Kind,
-						APIVersion: common.APIGroup + "/v1",
+						APIVersion: policiesv1.GroupVersion.String(),
 					},
 					Name:       dep.Namespace + "." + string(plc),
+					Namespace:  "",
 					Compliance: dep.Compliance,
 				})
 			}
-		} else if dep.Kind == policiesv1.Kind && dep.APIVersion == common.APIGroup+"/v1" {
-			if dep.Namespace == "" {
-				dep.Namespace = defaultNamespace
-			}
+		} else if depIsPolicy(dep) {
+			split := strings.Split(dep.Name, ".")
+			if len(split) == 2 { // assume it's already in the correct <namespace>.<name> format
+				deps = append(deps, dep)
+			} else {
+				if dep.Namespace == "" {
+					// use the namespace from the dependent policy when otherwise not provided
+					dep.Namespace = defaultNamespace
+				}
 
-			deps = append(deps, policiesv1.PolicyDependency{
-				TypeMeta: v1.TypeMeta{
-					Kind:       policiesv1.Kind,
-					APIVersion: common.APIGroup + "/v1",
-				},
-				Name:       dep.Namespace + "." + dep.Name,
-				Compliance: dep.Compliance,
-			})
+				dep.Name = dep.Namespace + "." + dep.Name
+				dep.Namespace = ""
+
+				deps = append(deps, dep)
+			}
 		} else {
 			deps = append(deps, dep)
 		}
@@ -145,26 +169,13 @@ func (r *PolicyReconciler) canonicalizeDependencies(
 	return deps, nil
 }
 
+// getPolicySetDependencies find all dependencies and extraDependencies in the given policy that
+// are PolicySets, since those objects will need to be watched.
 func getPolicySetDependencies(root *policiesv1.Policy) map[k8sdepwatches.ObjectIdentifier]bool {
 	policySetIDs := make(map[k8sdepwatches.ObjectIdentifier]bool)
 
 	for _, dep := range root.Spec.Dependencies {
-		ns := dep.Namespace
-		if ns == "" {
-			ns = root.Namespace
-		}
-
-		policySetIDs[k8sdepwatches.ObjectIdentifier{
-			Group:     common.APIGroup,
-			Version:   "v1beta1",
-			Kind:      policiesv1.PolicySetKind,
-			Namespace: ns,
-			Name:      dep.Name,
-		}] = true
-	}
-
-	for _, tmpl := range root.Spec.PolicyTemplates {
-		for _, dep := range tmpl.ExtraDependencies {
+		if depIsPolicySet(dep) {
 			ns := dep.Namespace
 			if ns == "" {
 				ns = root.Namespace
@@ -177,6 +188,25 @@ func getPolicySetDependencies(root *policiesv1.Policy) map[k8sdepwatches.ObjectI
 				Namespace: ns,
 				Name:      dep.Name,
 			}] = true
+		}
+	}
+
+	for _, tmpl := range root.Spec.PolicyTemplates {
+		for _, dep := range tmpl.ExtraDependencies {
+			if depIsPolicySet(dep) {
+				ns := dep.Namespace
+				if ns == "" {
+					ns = root.Namespace
+				}
+
+				policySetIDs[k8sdepwatches.ObjectIdentifier{
+					Group:     common.APIGroup,
+					Version:   "v1beta1",
+					Kind:      policiesv1.PolicySetKind,
+					Namespace: ns,
+					Name:      dep.Name,
+				}] = true
+			}
 		}
 	}
 
