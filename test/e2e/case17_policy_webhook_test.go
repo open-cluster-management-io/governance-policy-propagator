5adbb99b58ba1665a4b400078de6006b7e953228
Jason Zhang <jaszhang@redhat.com>
2023-10-25 10:42:01 -0400

    Update the webhook to validate the remediationAction field of policies
    Currently, when the remediationAction field is unset in both the root
    policy and configuration policy in its policy template, the violation
    is generated as an event by the config-policy controller in the
    managed cluster.
    
    This PR addresses the issue by updating the validating webhook so that
    the violation can be caught on policy creation and updates on the hub.
    
    ref: https://issues.redhat.com/browse/ACM-7551
    Signed-off-by: Jason Zhang <jaszhang@redhat.com>

--- test/e2e/case17_policy_webhook_test.go
+++ test/e2e/case17_policy_webhook_test.go
@@ -15,45 +15,70 @@ import (
 )
 
 const (
-	case17PolicyLongYaml       string = "../resources/case17_policy_webhook/case17_policy_long.yaml"
-	case17PolicyReplicatedYaml string = "../resources/case17_policy_webhook/" +
-		"case17_policy_replicate.yaml"
-	longNamesapce              string = "long-long-long-long-long-long-long"
-	case17PolicyReplicatedName string = "case17-test-policy-replicated-longlong"
-	errMsg                     string = `admission webhook "policy.open-cluster-management.io.webhook" denied the ` +
-		`request: the combined length of the policy namespace and name ` +
-		`cannot exceed 62 characters`
+	case17Prefix                     string = "../resources/case17_policy_webhook/"
+	case17PolicyLongYaml             string = case17Prefix + "case17_policy_long.yaml"
+	case17PolicyReplicatedYaml       string = case17Prefix + "case17_policy_replicate.yaml"
+	case17PolicyRemediationYaml      string = case17Prefix + "case17_invalid_remediation_policy.yaml"
+	case17PolicyRootRemediationYaml  string = case17Prefix + "case17_valid_remediation_policy_root.yaml"
+	case17PolicyCfplcRemediationYaml string = case17Prefix + "case17_valid_remediation_policy_cfplc.yaml"
+	longNamespace                    string = "long-long-long-long-long-long-long"
+	case17PolicyReplicatedName       string = "case17-test-policy-replicated-longlong"
+	case17PolicyReplicatedPlr        string = "case17-test-policy-replicated-longlong-plr"
+	case17PolicyReplicatedPb         string = "case17-test-policy-replicated-longlong-pb"
+	case17PolicyRemediationName      string = "case17-test-policy-no-remediation"
+	case17PolicyRootRemediationName  string = "case17-test-policy-root-remediation"
+	case17PolicyCfplcRemediationName string = "case17-test-policy-cfplc-remediation"
+	errPrefix                        string = `admission webhook "policy.open-cluster-management.io.webhook" ` +
+		`denied the request: `
+	combinedLengthErr string = errPrefix + "the combined length of the policy namespace and name " +
+		"cannot exceed 62 characters"
+	remediationErr string = errPrefix + "RemediationAction field of the policy and policy template " +
+		"cannot both be unset"
 )
 
 var _ = Describe("Test policy webhook", Label("webhook"), Ordered, func() {
-	BeforeAll(func() {
-		_, err := utils.KubectlWithOutput("create",
-			"ns", longNamesapce,
-		)
-		Expect(err).ShouldNot(HaveOccurred())
-	})
-	AfterAll(func() {
-		// cleanup
-		_, err := utils.KubectlWithOutput("delete",
-			"ns", longNamesapce,
-			"--ignore-not-found",
-		)
-		Expect(err).ShouldNot(HaveOccurred())
-
-		_, err = utils.KubectlWithOutput("delete",
-			"policy", case17PolicyReplicatedName,
-			"-n", testNamespace,
-			"--ignore-not-found",
-		)
-		Expect(err).ShouldNot(HaveOccurred())
-	})
 	Describe("Test name + namespace over 63", func() {
+		BeforeAll(func() {
+			_, err := utils.KubectlWithOutput("create",
+				"ns", longNamespace,
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+		AfterAll(func() {
+			// cleanup
+			_, err := utils.KubectlWithOutput("delete",
+				"ns", longNamespace,
+				"--ignore-not-found",
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			_, err = utils.KubectlWithOutput("delete",
+				"policy", case17PolicyReplicatedName,
+				"-n", testNamespace,
+				"--ignore-not-found",
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			_, err = utils.KubectlWithOutput("delete",
+				"placementrule", case17PolicyReplicatedPlr,
+				"-n", testNamespace,
+				"--ignore-not-found",
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			_, err = utils.KubectlWithOutput("delete",
+				"placementbinding", case17PolicyReplicatedPb,
+				"-n", testNamespace,
+				"--ignore-not-found",
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+		})
 		It("Should the error message is presented", func() {
 			output, err := utils.KubectlWithOutput("apply",
 				"-f", case17PolicyLongYaml,
-				"-n", longNamesapce)
+				"-n", longNamespace)
 			Expect(err).Should(HaveOccurred())
-			Expect(output).Should(ContainSubstring(errMsg))
+			Expect(output).Should(ContainSubstring(combinedLengthErr))
 		})
 		It("Should replicated policy should not be validated", func() {
 			_, err := utils.KubectlWithOutput("apply",
@@ -80,6 +105,81 @@ var _ = Describe("Test policy webhook", Label("webhook"), Ordered, func() {
 				"managed1", true, defaultTimeoutSeconds,
 			)
 			Expect(plc).ToNot(BeNil())
+		})
+	})
+
+	Describe("The remediationAction field should not be unset in both root policy and policy templates", func() {
+		AfterAll(func() {
+			_, err := utils.KubectlWithOutput("delete",
+				"policy", case17PolicyRemediationName,
+				"-n", testNamespace,
+				"--ignore-not-found",
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			_, err = utils.KubectlWithOutput("delete",
+				"policy", case17PolicyRootRemediationName,
+				"-n", testNamespace,
+				"--ignore-not-found",
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			_, err = utils.KubectlWithOutput("delete",
+				"policy", case17PolicyCfplcRemediationName,
+				"-n", testNamespace,
+				"--ignore-not-found",
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		It("Should return a validation error when creating policies", func() {
+			By("Applying a policy where both the remediationAction of the root policy " +
+				"and the configuration policy in the policy templates are unset")
+			output, err := utils.KubectlWithOutput("apply",
+				"-f", case17PolicyRemediationYaml,
+				"-n", testNamespace)
+			Expect(err).Should(HaveOccurred())
+			Expect(output).Should(ContainSubstring(remediationErr))
+		})
+
+		It("Should not return a validation error when creating policies", func() {
+			By("Applying a policy where only the remediationAction of the " +
+				"root policy is set")
+			_, err := utils.KubectlWithOutput("apply",
+				"-f", case17PolicyRootRemediationYaml,
+				"-n", testNamespace)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			By("Applying a policy where only the remediationAction of the " +
+				"configuration policy in the policy templates is set")
+			_, err = utils.KubectlWithOutput("apply",
+				"-f", case17PolicyCfplcRemediationYaml,
+				"-n", testNamespace)
+			Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		It("Should return a validation error when updating policies", func() {
+			By("Patching a policy so that the remediationAction field of both " +
+				"the root policy and configuration policy is unset")
+
+			output, err := utils.KubectlWithOutput("patch", "policy",
+				case17PolicyRootRemediationName, "-n", testNamespace,
+				"--type=json", "-p", "[{'op': 'remove', 'path': '/spec/remediationAction'}]",
+			)
+
+			Expect(err).Should(HaveOccurred())
+			Expect(output).Should(ContainSubstring(remediationErr))
+		})
+
+		It("Should not return a validation error when updating policies", func() {
+			By("Patching a policy so that only the remediationAction field of the root policy is unset")
+
+			_, err := utils.KubectlWithOutput("patch", "policy",
+				case17PolicyRootRemediationName, "-n", testNamespace, "--type=json", "-p",
+				"[{'op': 'add', 'path': '/spec/remediationAction', 'value': 'inform'}]",
+			)
+
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
 })
