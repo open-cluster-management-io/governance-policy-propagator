bad22d414e79895468bf4c15aeb1f32c1853493b
Yi Rae Kim <yikim@redhat.com>
2024-02-14 11:30:36 -0500

    Fix flacky case19 e2e test issue
    Signed-off-by: Yi Rae Kim <yikim@redhat.com>

--- test/e2e/case19_rep_policy_placement_test.go
+++ test/e2e/case19_rep_policy_placement_test.go
@@ -230,12 +230,15 @@ var _ = Describe("Test replicated_policy controller and propagation", Ordered, S
 			clientHubDynamic, gvrPolicy, testNamespace+"."+case19PolicyName, "managed2", true, defaultTimeoutSeconds,
 		)
 		Expect(plc2).ToNot(BeNil())
-		afterString = utils.GetMetrics("controller_runtime_reconcile_total",
-			`controller=\"replicated-policy\"`,
-			`,result=\"success\"`,
-		)[0]
-		afterTotal, err = strconv.Atoi(afterString)
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(afterTotal - beforeTotal).Should(Equal(2))
+
+		Eventually(func(g Gomega) {
+			afterString = utils.GetMetrics("controller_runtime_reconcile_total",
+				`controller=\"replicated-policy\"`,
+				`,result=\"success\"`,
+			)[0]
+			afterTotal, err = strconv.Atoi(afterString)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(afterTotal - beforeTotal).Should(Equal(2))
+		}).Should(Succeed())
 	})
 })
