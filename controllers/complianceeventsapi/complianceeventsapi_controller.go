2b3fd1d11c862bde6ae5518bb2cb14a0895eb457
mprahl <mprahl@users.noreply.github.com>
2024-02-22 16:04:19 -0500

    Remove whitespace in the connectionURL in governance-policy-database
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

--- controllers/complianceeventsapi/complianceeventsapi_controller.go
+++ controllers/complianceeventsapi/complianceeventsapi_controller.go
@@ -374,7 +374,7 @@ func ParseDBSecret(dbSecret *corev1.Secret, tempDirPath string) (string, error) 
 	var err error
 
 	if dbSecret.Data["connectionURL"] != nil {
-		connectionURL, err = url.Parse(string(dbSecret.Data["connectionURL"]))
+		connectionURL, err = url.Parse(strings.TrimSpace(string(dbSecret.Data["connectionURL"])))
 		if err != nil {
 			err := fmt.Errorf(
 				"%w: failed to parse the connectionURL value, but not logging the error to protect sensitive data",
