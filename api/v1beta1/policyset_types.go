6280a9b098019549222d7f6ccf6a520a8f3f5a61
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-03-11 16:32:12 -0400

    Update CRD descriptions
    ref: https://issues.redhat.com/browse/ACM-8992
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- api/v1beta1/policyset_types.go
+++ api/v1beta1/policyset_types.go
@@ -5,56 +5,71 @@ package v1beta1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
 
-// A custom type is required since there is no way to have a kubebuilder marker
-// apply to the items of a slice.
+	policyv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
+)
 
 // +kubebuilder:validation:MinLength=1
 type NonEmptyString string
 
-// PolicySetSpec describes a group of policies that are related and
-// can be placed on the same managed clusters.
+// PolicySetSpec defines the group of policies to be included in the policy set.
 type PolicySetSpec struct {
-	// Description of this PolicySet.
+	// Description is the description of this policy set.
 	Description string `json:"description,omitempty"`
-	// Policies that are grouped together within the PolicySet.
-	// +kubebuilder:validation:Required
+
+	// Policies is a list of policy names that are contained within the policy set.
 	Policies []NonEmptyString `json:"policies"`
 }
 
-// PolicySetStatus defines the observed state of PolicySet
-type PolicySetStatus struct {
-	Placement     []PolicySetStatusPlacement `json:"placement,omitempty"`
-	Compliant     string                     `json:"compliant,omitempty"`
-	StatusMessage string                     `json:"statusMessage,omitempty"`
-}
-
-// PolicySetStatusPlacement defines a placement object for the status
+// PolicySetStatusPlacement reports how and what managed cluster placement resources are attached to
+// the policy set.
 type PolicySetStatusPlacement struct {
+	// PlacementBinding is the name of the PlacementBinding resource, from the
+	// policies.open-cluster-management.io API group, that binds the placement resource to the policy
+	// set.
 	PlacementBinding string `json:"placementBinding,omitempty"`
-	Placement        string `json:"placement,omitempty"`
-	PlacementRule    string `json:"placementRule,omitempty"`
+
+	// Placement is the name of the Placement resource, from the cluster.open-cluster-management.io
+	// API group, that is bound to the policy.
+	Placement string `json:"placement,omitempty"`
+
+	// PlacementRule (deprecated) is the name of the PlacementRule resource, from the
+	// apps.open-cluster-management.io API group, that is bound to the policy.
+	PlacementRule string `json:"placementRule,omitempty"`
 }
 
+// PolicySetStatus reports the observed status of the policy set resulting from its policies.
+type PolicySetStatus struct {
+	Placement []PolicySetStatusPlacement `json:"placement,omitempty"`
+
+	// Compliant reports the observed status resulting from the compliance of the policies within.
+	Compliant policyv1.ComplianceState `json:"compliant,omitempty"`
+
+	// StatusMessge reports the current state while determining the compliance of the policy set.
+	StatusMessage string `json:"statusMessage,omitempty"`
+}
+
+// PolicySet is the schema for the policysets API. A policy set is a logical grouping of policies
+// from the same namespace. The policy set is bound to a placement resource and applies the
+// placement to all policies within the set. The status reports the overall compliance of the set.
+//
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=policysets,scope=Namespaced
 // +kubebuilder:resource:path=policysets,shortName=plcset
 // +kubebuilder:printcolumn:name="Compliance state",type="string",JSONPath=".status.compliant"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
-// PolicySet is the Schema for the policysets API
 type PolicySet struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	// +kubebuilder:validation:Required
+
 	Spec   PolicySetSpec   `json:"spec"`
 	Status PolicySetStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
-
-// PolicySetList contains a list of PolicySet
+// PolicySetList contains a list of policy sets.
+//
+// +kubebuilder:object:root=true
 type PolicySetList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
