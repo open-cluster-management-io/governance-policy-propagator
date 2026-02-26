194032ed940a02f5c718ece85b9d9884d703896d
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-02-26 12:40:23 -0500

    chore: use context logger in the webhook
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- api/v1/policy_webhook.go
+++ api/v1/policy_webhook.go
@@ -9,16 +9,16 @@ import (
 	"errors"
 	"unicode/utf8"
 
+	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 var (
 	// log is for logging in this package.
-	policylog = logf.Log.WithName("policy-validating-webhook")
-	errName   = errors.New("the combined length of the policy namespace and name " +
+	errName = errors.New("the combined length of the policy namespace and name " +
 		"cannot exceed 62 characters")
 	errRemediation = errors.New("RemediationAction field of the policy and " +
 		"policy template cannot both be unset")
@@ -27,6 +27,15 @@ var (
 func (r *Policy) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr, &Policy{}).
 		WithValidator(&PolicyCustomValidator{}).
+		WithLogConstructor(func(base logr.Logger, req *admission.Request) logr.Logger {
+			log := base.WithName("policy-validating-webhook")
+
+			if req != nil {
+				log = log.WithValues("kind", req.Kind, "namespace", req.Namespace, "name", req.Name)
+			}
+
+			return log
+		}).
 		Complete()
 }
 
@@ -37,16 +46,16 @@ type PolicyCustomValidator struct{}
 var _ admission.Validator[*Policy] = &PolicyCustomValidator{}
 
 // ValidateCreate implements admission.Validator so a webhook will be registered for the type
-func (r *PolicyCustomValidator) ValidateCreate(_ context.Context, policy *Policy) (admission.Warnings, error) {
-	log := policylog.WithValues("policyName", policy.Name, "policyNamespace", policy.Namespace)
+func (r *PolicyCustomValidator) ValidateCreate(ctx context.Context, policy *Policy) (admission.Warnings, error) {
+	log := log.FromContext(ctx)
 	log.V(1).Info("Validate policy creation request")
 
-	err := policy.validateName()
+	err := policy.validateName(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	err = policy.validateRemediationAction()
+	err = policy.validateRemediationAction(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -56,12 +65,12 @@ func (r *PolicyCustomValidator) ValidateCreate(_ context.Context, policy *Policy
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type
 func (r *PolicyCustomValidator) ValidateUpdate(
-	_ context.Context, _, policy *Policy,
+	ctx context.Context, _, policy *Policy,
 ) (admission.Warnings, error) {
-	log := policylog.WithValues("policyName", policy.Name, "policyNamespace", policy.Namespace)
+	log := log.FromContext(ctx)
 	log.V(1).Info("Validate policy update request")
 
-	err := policy.validateRemediationAction()
+	err := policy.validateRemediationAction(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -75,8 +84,8 @@ func (r *PolicyCustomValidator) ValidateDelete(_ context.Context, _ *Policy) (ad
 }
 
 // validate the policy name and namespace length
-func (r *Policy) validateName() error {
-	log := policylog.WithValues("policyName", r.Name, "policyNamespace", r.Namespace)
+func (r *Policy) validateName(ctx context.Context) error {
+	log := log.FromContext(ctx)
 	log.V(1).Info("Validating the policy name through a validating webhook")
 
 	// replicated policies don't need pass this validation
@@ -95,8 +104,8 @@ func (r *Policy) validateName() error {
 }
 
 // validate the remediationAction field of the root policy and its policy templates
-func (r *Policy) validateRemediationAction() error {
-	log := policylog.WithValues("policyName", r.Name, "policyNamespace", r.Namespace)
+func (r *Policy) validateRemediationAction(ctx context.Context) error {
+	log := log.FromContext(ctx)
 	log.V(1).Info("Validating the Policy and ConfigurationPolicy remediationAction through a validating webhook")
 
 	if r.Spec.RemediationAction != "" {
